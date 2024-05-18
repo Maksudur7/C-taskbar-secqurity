64da13b4c1a962ff9c82433d3eef46ae4b462889
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-05-18 17:08:52 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+    delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool IsWindowVisible(IntPtr hWnd);
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern int GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetShellWindow();
+
+    static void Main(string[] args)
+    {
+        IntPtr shellWindow = GetShellWindow();
+        EnumWindows(delegate (IntPtr hWnd, IntPtr lParam)
+        {
+            if (hWnd == shellWindow) return true;
+            if (!IsWindowVisible(hWnd)) return true;
+
+            int length = GetWindowTextLength(hWnd);
+            if (length == 0) return true;
+
+            var builder = new System.Text.StringBuilder(length);
+            GetWindowText(hWnd, builder, length + 1);
+
+            Console.WriteLine($"Window Title: {builder.ToString()}");
+
+            return true;
+        }, IntPtr.Zero);
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern int GetWindowTextLength(IntPtr hWnd);
+}
